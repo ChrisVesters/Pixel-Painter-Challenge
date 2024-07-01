b8964085f5a8e73bb92cfd46d1b1e9677c2f1400
Chris Vesters <chris.vesters@outlook.com>
2024-07-01 12:33:47 +0200

    basic scprites and initial render

--- Assets/RenderGrid.cs
+++ Assets/RenderGrid.cs
@@ -1,43 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class RenderGrid : MonoBehaviour
 {
-	const int GRID_X = 10;
-	const int GRID_Y = 10;
-	// public GameObject prefab;
+	private const int GRID_X = 10;
+	private const int GRID_Y = 10;
 
-	public Tilemap gridTileMap;
-
-	// Start is called before the first frame update
 	void Start()
 	{
-		var tile = Resources.Load("Tile");
-		var square = Resources.Load("Tiles");
-		//Debug.Log("Tile name:" + tile.name);
-		//Debug.Log("Tile sprite:" + tile.sprite);
+		Tilemap tilemap = GetComponent<Tilemap>();
+		Tile whiteTile = Resources.Load<Tile>("Tiles/White");
 
-		Debug.Log(square);
-		Debug.Log(tile);
-
-		// TODO: get the actual tilemap.
-		// TODO: Get the individual tiles.
-		// TODO: use those to actually render stuff.
-
-		// GameObject tilemap = GameObject.Find("Tilemap");
 		for (int y = 0; y < GRID_Y; ++y)
 		{
 			for (int x = 0; x < GRID_X; ++x)
 			{
-				// gridTileMap.SetTile(new Vector3Int(x, y, 0), tile);
-				// Instantiate(prefab, new Vector3(x - GRID_X / 2, y - GRID_Y / 2, 0), Quaternion.identity, tilemap.transform);
+				tilemap.SetTile(new Vector3Int(x, y, 0), whiteTile);
 			}
 		}
-
-		gridTileMap.CompressBounds();
 	}
 
 	// Update is called once per frame
