8c235912adc5074147f9a13b04ae0e51088aaf8d
Chris Vesters <vesters.chris@outlook.com>
2024-07-07 07:35:33 +0200

    Render checkerboard pattern

--- Assets/RenderGrid.cs
+++ Assets/RenderGrid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,19 +7,35 @@ using UnityEngine.Tilemaps;
 
 public class RenderGrid : MonoBehaviour
 {
-	private const int GRID_X = 10;
-	private const int GRID_Y = 10;
+	private const int GRID_SIZE = 6;
 
 	void Start()
 	{
 		Tilemap tilemap = GetComponent<Tilemap>();
-		Tile whiteTile = Resources.Load<Tile>("Tiles/White");
 
-		for (int y = 0; y < GRID_Y; ++y)
+		Tile border = Resources.Load<Tile>("Borders/Red");
+		Tile fill = Resources.Load<Tile>("Cells/Black");
+		Tile fillEvent = Resources.Load<Tile>("Cells/White");
+
+		for (int y = -GRID_SIZE; y < GRID_SIZE; ++y)
 		{
-			for (int x = 0; x < GRID_X; ++x)
+			for (int x = -GRID_SIZE; x < GRID_SIZE; ++x)
 			{
-				tilemap.SetTile(new Vector3Int(x, y, 0), whiteTile);
+				Vector3Int pos = new Vector3Int(x, y, 0);
+
+				tilemap.SetTile(new Vector3Int(x, y, 10), border);
+				if (Math.Abs(x % 2) == Math.Abs(y % 2))
+				{
+					tilemap.SetTile(pos, fillEvent);
+				}
+				else
+				{
+					tilemap.SetTile(new Vector3Int(x, y, 0), fill);
+				}
+
+				Color c = new Color(255f, 255f, 0f, 1f);
+				tilemap.SetColor(pos, c);
+				tilemap.RefreshTile(pos);
 			}
 		}
 	}
