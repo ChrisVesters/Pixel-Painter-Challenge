53b3148b6d1beb46b2964cf7bfcb3b0d1c57c511
Chris Vesters <vesters.chris@outlook.com>
2024-07-21 07:24:51 +0200

    Initial highlighting approach.

--- Assets/RenderGrid.cs
+++ Assets/RenderGrid.cs
@@ -11,6 +11,10 @@ public class RenderGrid : MonoBehaviour
 	private const int GRID_SIZE_X = 6;
 	private const int GRID_SIZE_Y = 6;
 
+	private const int Z_LAYER_BORDER = -1;
+	private const int Z_LAYER_FILL = 0;
+	private const int Z_LAYER_HIGHLIGHT = 1;
+
 	private Tilemap tilemap;
 
 	private Vector3Int? coloringStartPoint;
@@ -28,8 +32,8 @@ public class RenderGrid : MonoBehaviour
 		{
 			for (int x = -GRID_SIZE_X; x < GRID_SIZE_X; ++x)
 			{
-				Vector3Int borderPos = new Vector3Int(x, y, 10);
-				Vector3Int pos = new Vector3Int(x, y, 0);
+				Vector3Int borderPos = new Vector3Int(x, y, Z_LAYER_BORDER);
+				Vector3Int pos = new Vector3Int(x, y, Z_LAYER_FILL);
 
 				tilemap.SetTile(borderPos, border);
 				tilemap.SetTileFlags(borderPos, TileFlags.None);
@@ -48,11 +52,20 @@ public class RenderGrid : MonoBehaviour
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
 		Vector3Int cellPos = tilemap.WorldToCell(worldPos);
 
+		// Change the painting mechanism.
+		// Click on pixels to create a shape.
+		// Note: Only horizontal, vertical and diagonal lines are allowed.
+		// Close the shape (and paint it) by clicking on the start pixel.
+		// Note: Single pixel paining is not possible and simply clears it.
+		// Note: You can only remove the last point (by clicking on it).
+		// Note only convex shapes are allowed
 		if (Input.GetMouseButtonDown(0))
 		{
 			if (IsValidTilePosition(cellPos))
 			{
 				coloringStartPoint = cellPos;
+
+				highlight(cellPos);
 			}
 		}
 
@@ -66,12 +79,52 @@ public class RenderGrid : MonoBehaviour
 					tilemap.SetColor(cellPos, Color.black);
 				}
 			}
+
+			if (coloringStartPoint.HasValue)
+			{
+				clearHightlight(coloringStartPoint.Value);
+			}
+
 			coloringStartPoint = null;
+		}
+
+		if (Input.GetMouseButton(0))
+		{
+			// TODO: if cursor out of bounds, clear highlight?
+			if (IsValidTilePosition(cellPos))
+			{
+				// Highlight up to here from the start.
+				highlight(cellPos);
+			}
 		}
 	}
 
 	private bool IsValidTilePosition(Vector3Int position)
 	{
 		return tilemap.GetTile(position) != null;
+	}
+
+	private bool isStraightLine(Vector3Int start, Vector3Int end)
+	{
+		return start.x == end.x || start.y == end.y;
+	}
+
+	private void highlight(Vector3Int cell)
+	{
+		Tile fill = Resources.Load<Tile>("Fill");
+
+		Color source = Color.yellow;
+		Color color = new Color(source.r, source.g, source.b, 0.5f);
+		Vector3Int highlightPos = new Vector3Int(cell.x, cell.y, Z_LAYER_HIGHLIGHT);
+
+		tilemap.SetTile(highlightPos, fill);
+		tilemap.SetTileFlags(highlightPos, TileFlags.None);
+		tilemap.SetColor(highlightPos, color);
+	}
+
+	private void clearHightlight(Vector3Int cell)
+	{
+		Vector3Int highlightPos = new Vector3Int(cell.x, cell.y, Z_LAYER_HIGHLIGHT);
+		tilemap.SetTile(highlightPos, null);
 	}
 }
